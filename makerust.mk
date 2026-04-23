438da52bae8e0afbcb46d19852cb0def6f21d619
Jan-Erik Rediger <janerik@fnordig.de>
2026-04-23 10:57:57 -0400

    Use cargo's scripting functionality to keep it all in a single file
    Well, except we first collect dependencies into a separate file to
    concatenate them together

--- makerust.mk
+++ makerust.mk
@@ -4,15 +4,18 @@
 	if [ "$$(head -1 .makerust/tmp.rs)" = "dep" ]; then \
 		bash -c "`tail -n +2 .makerust/tmp.rs`" >> .makerust/dep; \
 	else \
-		printf "[package]\nname=\"makerust\"\nversion=\"0.1.0\"\nedition=\"2021\"\n" > .makerust/Cargo.toml && \
-		printf "[[bin]]\npath=\"tmp.rs\"\nname=\"bin\"\n" >> .makerust/Cargo.toml && \
-		printf "[dependencies]\n" >> .makerust/Cargo.toml && \
+		printf "\#!/usr/bin/env cargo\n" > .makerust/tmp.rs && \
+		printf -- "---cargo\n" >> .makerust/tmp.rs && \
+		printf "package.edition = \"2024\"\n" >> .makerust/tmp.rs && \
+		printf "[dependencies]\n" >> .makerust/tmp.rs && \
 		if [ -f ".makerust/dep" ]; then \
 			for dep in $$(sort .makerust/dep | uniq); do \
-				printf "$${dep}=\"*\"\n" >> .makerust/Cargo.toml; \
+				printf "$${dep}=\"*\"\n" >> .makerust/tmp.rs; \
 			done; \
 		fi; \
-		cargo run -q --manifest-path .makerust/Cargo.toml; \
+		printf -- "---\n" >> .makerust/tmp.rs && \
+		echo "$$1" >> .makerust/tmp.rs && \
+		cargo +nightly -q -Zscript .makerust/tmp.rs && \
 		rm -f .makerust/dep; \
 	fi \
 ' --
